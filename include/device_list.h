08d0163cf6228b44390820aa0a3582d07937fea1
Timur Doumler <git@timur.audio>
2018-11-09 22:04:33 -0800

    Removed const iterator from device_list (currently not needed).

--- include/device_list.h
+++ include/device_list.h
@@ -26,25 +26,14 @@ private:
 public:
   /** A forward iterator whose value type is audio::device. */
   using iterator = _underlying_container::iterator;
-
-  /** A const forward iterator whose value type is audio::device. */
-  using const_iterator = _underlying_container::const_iterator;
-
+  
   /** Returns an iterator referring to the first audio::device in the list. */
   iterator begin() noexcept { return _dlist.begin(); }
-
-  /** Returns a const iterator referring to the first audio::device in the list. */
-  const_iterator cbegin() const noexcept { return _dlist.cbegin(); }
 
   /** Returns the past-the-end iterator of the device list. Attempting to dereference
    *  this iterator results in undefined behavior.
    */
   iterator end() noexcept { return _dlist.end(); }
-
-  /** Returns the past-the-end const iterator of the device list. Attempting to dereference
-   *  this iterator results in undefined behavior.
-   */
-  const_iterator cend() const noexcept { return _dlist.cend(); }
 
 private:
   device_list(_underlying_container&& dlist) : _dlist(std::move(dlist)) {}
