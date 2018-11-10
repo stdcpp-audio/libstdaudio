60d96eec5d673d0c180ae80401d7c099d7226ef7
Timur Doumler <git@timur.audio>
2018-11-09 16:48:30 -0800

    Added stub for buffer_list class.

---
+++ include/buffer_list.h
@@ -0,0 +1,13 @@
+// libstdaudio
+// Copyright (c) 2018 - Timur Doumler
+
+#include "config.h"
+
+LIBSTDAUDIO_NAMESPACE_BEGIN
+
+/** A list of audio input or output buffers. */
+class buffer_list {
+  // TODO
+};
+
+LIBSTDAUDIO_NAMESPACE_END
