5b0ba0633b09ce0ed61d6fcd2fafce899ea184f4
Timur Doumler <git@timur.audio>
2018-11-14 16:38:27 +0100

    Implemented level_meter example app (not working yet).

--- examples/level_meter.cpp
+++ examples/level_meter.cpp
@@ -1,15 +1,32 @@
 // libstdaudio
 // Copyright (c) 2018 - Timur Doumler
 
+#include <iostream>
+#include <atomic>
+#include <thread>
 #include <audio.h>
 
 int main() {
   using namespace std::experimental;
+  std::atomic<float> max_volume = 0;
 
   auto d = audio::get_input_device();
-  d.connect([](audio::device&, audio::buffer_list&){
-    // TODO: implement level meter here
+  d.connect([&](audio::device&, audio::buffer_list& bl){
+    float new_max_volume = 0;
+
+    for (auto& buffer : bl.input_buffers()) {
+      for (auto& sample : buffer.raw()) {
+        if (sample > new_max_volume)
+          new_max_volume = sample;
+      }
+    }
+
+    max_volume.store(new_max_volume);
   });
 
-  while(true);
+  while(true) {
+    max_volume.store(0);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    std::cout << max_volume << std::endl;
+  }
 }
