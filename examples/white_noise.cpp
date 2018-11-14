21db855a26c672c80262716b7ff158fb23b9f1f7
Timur Doumler <git@timur.audio>
2018-11-14 16:27:37 +0100

    Implemented working white_noise example app (currently only looping through raw buffer data, not through channels or frames).

--- examples/white_noise.cpp
+++ examples/white_noise.cpp
@@ -1,14 +1,21 @@
 // libstdaudio
 // Copyright (c) 2018 - Timur Doumler
 
+#include <random>
 #include <audio.h>
 
 int main() {
   using namespace std::experimental;
 
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<float> white_noise(-1.0f, 1.0f);
+
   auto d = audio::get_output_device();
-  d.connect([](audio::device&, audio::buffer_list&){
-    // TODO: implement white noise generator here
+  d.connect([&](audio::device&, audio::buffer_list& bl){
+    for (auto& buffer : bl.output_buffers())
+      for (auto& sample : buffer.raw())
+        sample = white_noise(gen);
   });
 
   while(true);
