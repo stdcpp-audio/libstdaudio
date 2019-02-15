cfd7de499d0a51135372a50c5714a7bd7869a481
Timur Doumler <git@timur.audio>
2019-02-15 16:34:12 +0000

    Added printing a device's buffer order, sample rate, and buffer size to print_devices example app.

--- examples/print_devices.cpp
+++ examples/print_devices.cpp
@@ -7,7 +7,10 @@
 using namespace std::experimental;
 
 void print_device_info(audio::device& d) {
-  std::cout << "- \"" << d.name() << "\"\n";
+  std::cout << "- \"" << d.name() << "\", ";
+  std::cout << (d.get_native_order() == audio::buffer_order::deinterleaved ? "de" : "") << "interleaved, ";
+  std::cout << "sample rate = " << d.get_sample_rate() << " Hz, ";
+  std::cout << "buffer size = " << d.get_buffer_size() << " bytes\n";
 };
 
 void print_device_list(audio::device_list& list) {
