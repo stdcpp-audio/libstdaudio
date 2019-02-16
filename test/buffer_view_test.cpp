36f4e345a732b314efabed8a206edde972f8b59d
Timur Doumler <git@timur.audio>
2019-02-16 12:30:31 +0000

    buffer_view: empty() and size() for empty buffer.

--- test/buffer_view_test.cpp
+++ test/buffer_view_test.cpp
@@ -9,9 +9,13 @@ using namespace std::experimental::audio;
 TEST_CASE( "Construct channel view from empty buffer", "[buffer_view]") {
   buffer b;
   channel_view channels(b);
+  REQUIRE(channels.empty());
+  REQUIRE(channels.size() == 0);
 }
 
 TEST_CASE( "Construct frame view from empty buffer", "[buffer_view]") {
   buffer b;
   frame_view frames(b);
+  REQUIRE(frames.empty());
+  REQUIRE(frames.size() == 0);
 }
