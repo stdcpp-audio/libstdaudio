df997708183816e0216dfb595cab575dc12a4113
Timur Doumler <git@timur.audio>
2019-02-16 07:09:07 -0800

    Implemented buffer_view::operator[] for interleaved buffers; switched size type of buffer, buffer_view, and strided_span to signed for consistency with std::span.

--- test/buffer_view_test.cpp
+++ test/buffer_view_test.cpp
@@ -83,11 +83,45 @@ TEST_CASE( "Multi-channel deinterleaved channel view size", "[buffer_view]") {
   REQUIRE(channels.size() == 3);
 }
 
-TEST_CASE( "Multie-channel deinterleaved frame view size", "[buffer_view]") {
+TEST_CASE( "Multi-channel deinterleaved frame view size", "[buffer_view]") {
   float data[] = {1, 2, 3, 4, 5, 6};
   auto buf = buffer(data, 3, buffer_order::deinterleaved);
 
   auto frames = buf.frames();
   REQUIRE(!frames.empty());
   REQUIRE(frames.size() == 2);
+}
+
+TEST_CASE( "Interleaved channel view elements", "[buffer_view]") {
+  float data[] = {1, 2, 3, 4, 5, 6};
+  auto buf = buffer(data, 3, buffer_order::interleaved);
+
+  auto channels = buf.channels();
+  REQUIRE(channels[0].size() == 2);
+  REQUIRE(channels[0][0] == 1);
+  REQUIRE(channels[0][1] == 4);
+
+  REQUIRE(channels[1].size() == 2);
+  REQUIRE(channels[1][0] == 2);
+  REQUIRE(channels[1][1] == 5);
+
+  REQUIRE(channels[2].size() == 2);
+  REQUIRE(channels[2][0] == 3);
+  REQUIRE(channels[2][1] == 6);
+}
+
+TEST_CASE( "Interleaved frame view elements", "[buffer_view]") {
+  float data[] = {1, 2, 3, 4, 5, 6};
+  auto buf = buffer(data, 3, buffer_order::interleaved);
+
+  auto frames = buf.frames();
+  REQUIRE(frames[0].size() == 3);
+  REQUIRE(frames[0][0] == 1);
+  REQUIRE(frames[0][1] == 2);
+  REQUIRE(frames[0][2] == 3);
+
+  REQUIRE(frames[1].size() == 3);
+  REQUIRE(frames[1][0] == 4);
+  REQUIRE(frames[1][1] == 5);
+  REQUIRE(frames[1][2] == 6);
 }
