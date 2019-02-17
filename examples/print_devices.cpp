92caaf9cb8335e40de35a4a64437b39de1100804
Timur Doumler <git@timur.audio>
2019-02-17 13:30:17 -0800

    Added LICENSE.md. Added license note to the header comment of every source file.

--- examples/print_devices.cpp
+++ examples/print_devices.cpp
@@ -1,5 +1,7 @@
 // libstdaudio
 // Copyright (c) 2018 - Timur Doumler
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 
 #include <iostream>
 #include <audio>
--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,5 +1,7 @@
 // libstdaudio
 // Copyright (c) 2018 - Timur Doumler
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 
 #define CATCH_CONFIG_MAIN
 #include "catch/catch.hpp"
