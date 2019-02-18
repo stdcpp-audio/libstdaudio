1abf99bc352580c774d51ba196420f24aadc5b5f
Timur Doumler <git@timur.audio>
2019-02-17 18:54:35 -1000

    Reworked the level_meter example app to print the input volume peak in dB every 250 ms.

--- examples/level_meter.cpp
+++ examples/level_meter.cpp
@@ -4,31 +4,36 @@
 // (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 
 #include <iostream>
+#include <iomanip>
 #include <atomic>
+#include <cmath>
 #include <thread>
 #include <audio>
 
+// This example app prints the current input level in regular intervals.
+
+float gain_to_db(float gain) noexcept {
+  return gain > 0 ? std::log10(gain) * 20.0f : -std::numeric_limits<float>::infinity();
+}
+
 int main() {
   using namespace std::experimental::audio;
-  std::atomic<float> max_volume = 0;
+  std::atomic<float> max_abs_value = 0;
 
   auto d = get_default_input_device();
   d.connect([&](device&, buffer_list& bl){
-    float new_max_volume = 0;
-
     for (auto& buffer : bl.input_buffers()) {
       for (auto& sample : buffer.raw()) {
-        if (sample > new_max_volume)
-          new_max_volume = sample;
+        float abs_value = std::abs(sample);
+        if (abs_value > max_abs_value)
+          max_abs_value.store(abs_value);
       }
     }
-
-    max_volume.store(new_max_volume);
   });
 
+  d.start();
   while(true) {
-    max_volume.store(0);
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    std::cout << max_volume << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(250));
+    std::cout << gain_to_db(max_abs_value.exchange(0)) << " dB\n";
   }
 }
