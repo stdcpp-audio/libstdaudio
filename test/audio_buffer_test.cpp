687fa54ee323d94958d4f48f801e06c5a9e1d84b
Timur Doumler <git@timur.audio>
2019-05-30 19:43:17 +0100

    Added file for audio_buffer tests.

---
+++ test/audio_buffer_test.cpp
@@ -0,0 +1,10 @@
+// libstdaudio
+// Copyright (c) 2018 - Timur Doumler
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+#include <audio>
+#include "catch/catch.hpp"
+
+using namespace std::experimental;
+
